720642acbd04f4f16ca595a50786c8eab360edcf
unknown <jerzy.kwiatkowski@yahoo.pl>
2020-04-04 02:23:52 +0200

    hw

---
+++ rankall.R
@@ -0,0 +1,113 @@
+
+rankall <- function( outcome = 'pneumonia', num = "worst" ) {
+    library(data.table)
+    library(dplyr)
+    
+    
+    care_measures_data <- data.table(read.csv2('H:\\LITERATURA_i_POBRANE\\R_kurs\\Cursera R\\R-programming-week-4\\outcome-of-care-measures.csv', sep = ',', dec ='.', colClasses = "character" ))
+    
+ 
+    
+    ## Check that state and outcome are valid
+    
+    # New names
+    
+    old <- care_measures_data %>% 
+                            select( starts_with("Hospital.30.Day.Death.")) %>% 
+                            names()
+ 
+    # old <- c('Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure' ,
+    #          
+    #          'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia')
+    
+    new <- c('heart attack' , 'heart failure' , 'pneumonia')
+    
+    setnames(care_measures_data , old , new)
+    
+    
+    
+    care_measures_data_2 <- care_measures_data %>%
+        
+        select( Hospital.Name, State, outcome ) 
+    
+    
+    # cleaning 
+  
+    
+    
+    # min if else 
+    if (outcome == 'heart attack'){
+      care_measures_data_2[`heart attack` == "Not Available", `heart attack`:= "NA"]
+      care_measures_data_2$`heart attack` <- as.numeric(care_measures_data_2$`heart attack`)
+      care_measures_data_2 <- na.omit(care_measures_data_2)
+      care_measures_data_2 <- data.table(care_measures_data_2)  
+      
+    care_measures_data_2 <- care_measures_data_2[order(State , `heart attack`)]
+    care_measures_data_2 <- care_measures_data_2[,Rank := round(frank(`heart attack`) , 0), by = .(State)]
+
+    if( num == 'best'){
+      outcome_final <-  care_measures_data_2[Rank == 1,] 
+    }else if(num == "worst"){
+      outcome_final <-   care_measures_data_2[rank_worst == Rank,] 
+      
+    }else{
+      outcome_final <-  care_measures_data_2[Rank == num,] 
+      
+    }
+   
+    
+    } else if (outcome == 'heart failure'){
+       
+      care_measures_data_2[`heart failure` == "Not Available", `heart failure`:= "NA"]
+      care_measures_data_2$`heart failure` <- as.numeric(care_measures_data_2$`heart failure`)
+      care_measures_data_2 <- na.omit(care_measures_data_2)
+      care_measures_data_2 <- data.table(care_measures_data_2)  
+      
+      care_measures_data_2 <- care_measures_data_2[order(State , `heart failure`)]
+      care_measures_data_2 <- care_measures_data_2[,Rank := round(frank(`heart failure`) , 0), by = .(State)]
+      
+      if( num == 'best'){
+        outcome_final <-  care_measures_data_2[Rank == 1,] 
+      }else if(num == "worst"){
+        outcome_final <-   care_measures_data_2[rank_worst == Rank,] 
+        
+      }else{
+        outcome_final <-  care_measures_data_2[Rank == num,] 
+        
+      }
+        
+    } else{
+        
+        
+      care_measures_data_2[`pneumonia` == "Not Available", `pneumonia`:= "NA"]
+      care_measures_data_2$`pneumonia` <- as.numeric(care_measures_data_2$`pneumonia`)
+      care_measures_data_2 <- na.omit(care_measures_data_2)
+      care_measures_data_2 <- data.table(care_measures_data_2)  
+      
+      care_measures_data_2 <- care_measures_data_2[order(State , `pneumonia`)]
+      care_measures_data_2 <- care_measures_data_2[,Rank := round(frank(`pneumonia`) , 0), by = .(State)]
+      care_measures_data_2[, rank_worst := max(Rank), by = .(State)]
+      care_measures_data_2[order(Rank, Hospital.Name)]
+      
+      if( num == 'best'){
+        outcome_final <-  care_measures_data_2[Rank == 1,] 
+      }else if(num == "worst"){
+        outcome_final <-   care_measures_data_2[rank_worst == Rank,] 
+        
+      }else{
+        outcome_final <-  care_measures_data_2[Rank == num,] 
+        
+      }
+        
+    }
+        
+     # final    
+    
+    return(outcome_final)
+   
+  
+    
+    
+# end function
+}
+
---
+++ rankall_test.R
@@ -0,0 +1,10 @@
+source("H:\\LITERATURA_i_POBRANE\\R_kurs\\Cursera R\\R-programming-week-4\\rankall.R")
+head(rankall("heart attack", 20), 10)
+
+
+
+tail(rankall("pneumonia", "worst"), 3)
+hospital state
+WI MAYO CLINIC HEALTH SYSTEM - NORTHLAND, INC WI
+WV PLATEAU MEDICAL CENTER WV
+WY NORTH BIG HORN HOSPITAL DISTRICT WY
